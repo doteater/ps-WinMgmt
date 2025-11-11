67886b2a26197527c3c29458e21e1e2742898a52
doteater <3267474+doteater@users.noreply.github.com>
2025-11-11 15:30:23 -0700

    Create Apply-Policies.psm1

---
+++ Modules/Apply-Policies/Apply-Policies.psm1
@@ -0,0 +1,22 @@
+#prevent connecting to MS account
+Set-RegistryValue "HKEY_LOCAL_MACHINE" "Software\Policies\Microsoft\MicrosoftAccount" "DisableUserAuth" 1 DWord
+Set-RegistryValue "HKEY_LOCAL_MACHINE" "SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" "NoConnectedUser" 3 DWord
+Set-RegistryValue "HKEY_LOCAL_MACHINE" "SOFTWARE\Microsoft\PolicyManager\default\Settings\AllowYourAccount" "value" 0 DWord
+
+#prevent enabling bitlocker
+Set-RegistryValue "HKEY_LOCAL_MACHINE" "SYSTEM\CurrentControlSet\Control\BitLocker" "PreventDeviceEncryption" 1 DWord
+
+#update time zone automatically
+Set-RegistryValue "HKEY_LOCAL_MACHINE" "SYSTEM\CurrentControlSet\Services\tzautoupdate" "Start" 3 DWord
+
+#disable MS Office MS Account sign-in button
+Set-RegistryValue "HKEY_CURRENT_USER" "SOFTWARE\Microsoft\Office\16.0\Common\SignIn" "SignInOptions" 3 DWord
+Set-RegistryValue "HKEY_CURRENT_USER" "SOFTWARE\Microsoft\Office\16.0\Common\SignIn" "SignInOptions" 3 DWord -AllUsers -IncludeDefault
+
+
+#add ublock on chrome and edge
+set-executionpolicy unrestricted -scope process
+irm https://raw.githubusercontent.com/cory-sc/Manage-Browser-Extensions/refs/heads/main/Manage-Browser-Extensions.ps1 -o C:\windows\temp\mbe.ps1
+. C:\windows\temp\mbe.ps1
+Force-InstallExtension -Browser "Chrome" -ExtensionID "ddkjiahejlhfcafbddmgiahcphecmpfh"
+Force-InstallExtension -Browser "Edge" -ExtensionID "odfafepnkmbhccpbejgmiehpchacaeak"
