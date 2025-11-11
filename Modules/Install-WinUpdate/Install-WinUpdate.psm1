30cbc850bfcf97a0eaab4d11a474e085cbbd1781
doteater <3267474+doteater@users.noreply.github.com>
2025-11-11 13:14:25 -0700

    Create Install-WinUpdate.psm1

---
+++ Modules/Install-WinUpdate/Install-WinUpdate.psm1
@@ -0,0 +1,15 @@
+
+Install-PackageProvider -name nuget -force
+
+$moduleName = 'PSWindowsUpdate'
+if (-not (Get-Module -ListAvailable -Name $moduleName)) {
+Install-Module $moduleName -Force -SkipPublisherCheck
+}
+
+#----
+Set-ExecutionPolicy Unrestricted -Scope Process
+
+Import-Module PSWindowsUpdate
+
+#bring WU up to date
+Install-WindowsUpdate -AcceptAll -Download -IgnoreReboot -Install
