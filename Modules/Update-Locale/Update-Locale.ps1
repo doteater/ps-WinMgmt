a537381aae8083e97411b4791394fcfe86949ddf
doteater <3267474+doteater@users.noreply.github.com>
2025-11-11 12:27:23 -0700

    Update Update-Locale.ps1

--- Modules/Update-Locale/Update-Locale.ps1
+++ Modules/Update-Locale/Update-Locale.ps1
@@ -4,14 +4,16 @@ function Update-Locale {
         #[Parameter(Mandatory = $true)]
         #[bool]$Enable
     #)
+    Write-Host "=== Update-Locale Script ==="
+    Write-Host "installing language"
     Install-Language -Language en-US
 
-
+    Write-Host "set system locale, ui language"
     Set-WinSystemLocale en-US
     Set-SystemPreferredUILanguage -Language en-US
     Set-WinUserLanguageList en-US -Force
     
-    
+    Write-Host "apply to new users"
     Copy-UserInternationalSettingsToSystem -WelcomeScreen $true -NewUser $true
 
 }
