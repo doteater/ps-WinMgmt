9bfb30f0bda6c84b964dd6d3b1a7514b8e17cfb3
Cory Johnson <cory.johnson@sentraca.com>
2025-11-21 15:22:40 -0700

    adding module wrapping lsu-update ps module

---
+++ Modules/Update-LenovoTVSU/Update-LenovoTVSU.psm1
@@ -0,0 +1,13 @@
+function Update-LenovoTVSU {
+
+    Set-ExecutionPolicy Unrestricted
+    if (-not (Get-PSRepository -Name 'PSGallery' -ErrorAction SilentlyContinue)) {
+        Register-PSRepository -Default
+    }
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+    Install-Module -Name 'LSUClient'
+    $updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
+    $updates | Save-LSUpdate -Verbose
+    $updates | Install-LSUpdate -Verbose
+}
