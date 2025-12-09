5131044616b61c37e422cb586cdb3e8992e61b92
Cory Johnson <cory.johnson@sentraca.com>
2025-12-09 15:15:40 -0700

    ?

---
+++ Modules/Install-Conda/Install-Conda.psm1
@@ -0,0 +1,8 @@
+winget install anaconda.miniconda3
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%PATH%;C:\programdata\miniconda3\scripts\" /f
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+conda install -c conda-forge glances
+
+
