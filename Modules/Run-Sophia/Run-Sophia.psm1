c5c474583e3e0dd3f14807846239e07b7fe25877
doteater <3267474+doteater@users.noreply.github.com>
2026-03-05 10:43:45 -0700

    Update Run-Sophia.psm1

--- Modules/Run-Sophia/Run-Sophia.psm1
+++ Modules/Run-Sophia/Run-Sophia.psm1
@@ -26,7 +26,7 @@ function Run-Sophia {
     Elevate-Script
     
     
-    $URL = 'https://github.com/doteater/ps-WinMgmt/raw/refs/heads/main/sc-sofia-7.1.1-260217.zip'
+    $URL = 'https://github.com/doteater/ps-WinMgmt/raw/refs/heads/main/sc-sofia-7.1.4.zip'
     $filename = 'C:\windows\temp\s.zip'
     irm $URL -o $filename
     $timestamp = Get-Date -Format "yyyyMMddHHmmss"
