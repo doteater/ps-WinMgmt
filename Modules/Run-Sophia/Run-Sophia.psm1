160c58bc4572ef1a2052ac2ac5c5ad3f6afbfa2d
crochet <cory.johnson@sentracam.com>
2026-04-16 11:29:06 -0700

    sophia cursor typo fix

--- Modules/Run-Sophia/Run-Sophia.psm1
+++ Modules/Run-Sophia/Run-Sophia.psm1
@@ -1,6 +1,6 @@
 set-executionpolicy unrestricted -scope process
 function Run-Sophia {
-    $sophiazip = "sc-sophia-7.1.5-260416.3.zip"
+    $sophiazip = "sc-sophia-7.1.5-260416.4.zip"
     $ep = get-executionpolicy
     
     #write "ep: $ep"
