853e20a19183730502a28b7e7ae169f8fe6812a9
crochet <cory.johnson@sentracam.com>
2026-08-12 17:24:30 -0700

    sophia update aug 26 mk3

--- Modules/Run-Sophia/Run-Sophia.psm1
+++ Modules/Run-Sophia/Run-Sophia.psm1
@@ -3,7 +3,7 @@ function Run-Sophia {
     #$sophiazip = "sc-sophia-7.1.5-260416.4.zip"
     #$sophiazip = "sc-sophia-7.1.6-260625.3.zip"
 ##    $sophiazip = "sc-sophia-260625.8.zip"
-    $sophiazip = "sc-sophia-260812.0.zip"
+    $sophiazip = "sc-sophia-260812.1.zip"
     $ep = get-executionpolicy
     
     #write "ep: $ep"
